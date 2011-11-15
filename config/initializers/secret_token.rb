d99f8c56e95c04518fad39d10d6fa4ba3e2ff490
Steve McLelland <steve@2paperdolls.com>
2011-11-15 13:17:18 +0000

    d

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ec2test2::Application.config.secret_token = 'cf6939d0aa61849289d992ca0db763251a82de274753fd6e5f6e541aa9798ab71c342bdf58466919e913ec688d710bf0332fa6df57fa53e38febdd3c2ad74c7e'
